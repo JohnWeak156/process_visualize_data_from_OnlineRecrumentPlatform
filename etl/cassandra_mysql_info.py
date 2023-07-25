0050b01cb1c3eba5b602f90c7e3cc3af30a87c76
hauct <cungtronghau@gmail.com>
2023-07-25 08:59:49 +0700

    upload files

---
+++ etl/cassandra_mysql_info.py
@@ -0,0 +1,13 @@
+# MySQL
+MY_HOST = 'localhost'
+MY_PORT = '3306'
+MY_DBNAME = 'study_de'
+MY_URL = 'jdbc:mysql://' + MY_HOST + ':' + MY_PORT + '/' + MY_DBNAME
+
+MY_DRIVER = 'com.mysql.cj.jdbc.Driver'
+MY_USER = 'root'
+MY_PASSWORD = ''
+
+# Cassandra
+CAS_TABLE = 'log_tracking'
+CAS_KEYSPACE = 'study_de'
---
+++ etl/export_CS_to_MySQL.py
@@ -0,0 +1,189 @@
+# Import các thư viện
+import findspark
+findspark.init()
+
+import pyspark
+import pyspark.sql as pyspark_sql
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from pyspark.sql.window import Window
+from pyspark.sql.functions import udf
+
+import pandas as pd
+import uuid
+import time_uuid
+from datetime import datetime
+
+from cassandra_mysql_info import MY_HOST, MY_PORT, MY_DBNAME, MY_URL, MY_DRIVER, MY_USER, MY_PASSWORD, CAS_TABLE, CAS_KEYSPACE 
+
+# Tạo và cài đặt Spark
+from pyspark.sql import SparkSession
+
+#--- Kết nối tới Cassandra để lấy bảng tracking---
+data = spark.read.format("org.apache.spark.sql.cassandra")\
+    .options(table=CAS_TABLE, keyspace=CAS_KEYSPACE)\
+    .load()\
+    .select('create_time', col('job_id').cast(IntegerType()).cast(StringType()), 'custom_track','bid','campaign_id'\
+            ,col('group_id').cast(IntegerType()).cast(StringType()), 'publisher_id', 'ts')
+
+# Dùng create_time (Chuyển từ dạng uuid time -> timestamp)
+def uuid2ts(uuid_str):
+    my_uuid = uuid.UUID(uuid_str)
+    ts_long = time_uuid.TimeUUID(bytes=my_uuid.bytes).get_timestamp()
+    return float(ts_long)
+
+uuid2ts_udf = udf(uuid2ts, FloatType())
+
+def process_df(df):
+    df_processed = df\
+    .withColumn('ts', from_unixtime(uuid2ts_udf('create_time')))\
+    .select('create_time', 'ts', 'job_id','custom_track','bid','campaign_id','group_id','publisher_id')
+    return df_processed
+    
+## Tính toán các bảng output
+## Bảng `click`
+def calculating_clicks(df):
+    clicks_data = df.filter(df.custom_track == 'click')
+    clicks_data = clicks_data.na.fill({'bid':0})
+    clicks_data = clicks_data.na.fill({'job_id':0})
+    clicks_data = clicks_data.na.fill({'publisher_id':0})
+    clicks_data = clicks_data.na.fill({'group_id':0})
+    clicks_data = clicks_data.na.fill({'campaign_id':0})
+    clicks_data.createOrReplaceTempView('clicks')
+    clicks_output = spark.sql("""select job_id , date(ts) as date , hour(ts) as hour , publisher_id , campaign_id , group_id , avg(bid) as bid_set, count(*) as clicks , sum(bid) as spend_hour from clicks
+    group by job_id , date(ts) , hour(ts) , publisher_id , campaign_id , group_id """)
+    return clicks_output 
+
+## Bảng `coversion`
+def calculating_conversion(df):
+    conversion_data = df.filter(df.custom_track == 'conversion')
+    conversion_data = conversion_data.na.fill({'job_id':0})
+    conversion_data = conversion_data.na.fill({'publisher_id':0})
+    conversion_data = conversion_data.na.fill({'group_id':0})
+    conversion_data = conversion_data.na.fill({'campaign_id':0})
+    conversion_data.createOrReplaceTempView('conversion')
+    conversion_output = spark.sql("""select job_id , date(ts) as date , hour(ts) as hour , publisher_id , campaign_id , group_id , count(*) as conversions  from conversion
+    group by job_id , date(ts) , hour(ts) , publisher_id , campaign_id , group_id """)
+    return conversion_output 
+
+## Bảng `qualifed`
+def calculating_qualified(df):    
+    qualified_data = df.filter(df.custom_track == 'qualified')
+    qualified_data = qualified_data.na.fill({'job_id':0})
+    qualified_data = qualified_data.na.fill({'publisher_id':0})
+    qualified_data = qualified_data.na.fill({'group_id':0})
+    qualified_data = qualified_data.na.fill({'campaign_id':0})
+    qualified_data.createOrReplaceTempView('qualified')
+    qualified_output = spark.sql("""select job_id , date(ts) as date , hour(ts) as hour , publisher_id , campaign_id , group_id , count(*) as qualified  from qualified
+    group by job_id , date(ts) , hour(ts) , publisher_id , campaign_id , group_id """)
+    return qualified_output
+
+## Bảng `ununqualified`
+def calculating_unqualified(df):
+    unqualified_data = df.filter(df.custom_track == 'unqualified')
+    unqualified_data = unqualified_data.na.fill({'job_id':0})
+    unqualified_data = unqualified_data.na.fill({'publisher_id':0})
+    unqualified_data = unqualified_data.na.fill({'group_id':0})
+    unqualified_data = unqualified_data.na.fill({'campaign_id':0})
+    unqualified_data.createOrReplaceTempView('unqualified')
+    unqualified_output = spark.sql("""select job_id , date(ts) as date , hour(ts) as hour , publisher_id , campaign_id , group_id , count(*) as unqualified  from unqualified
+    group by job_id , date(ts) , hour(ts) , publisher_id , campaign_id , group_id """)
+    return unqualified_output
+
+# Join các bảng trên để ra kết quả
+def process_final_data(clicks_output,conversion_output,qualified_output,unqualified_output):
+    keys = ['job_id','date','hour','publisher_id','campaign_id','group_id']
+    final_data = clicks_output\
+    .join(conversion_output, keys,'full')\
+    .join(qualified_output, keys,'full')\
+    .join(unqualified_output, keys,'full')
+    return final_data 
+
+def process_cassandra_data(df):
+    clicks_output = calculating_clicks(df)
+    conversion_output = calculating_conversion(df)
+    qualified_output = calculating_qualified(df)
+    unqualified_output = calculating_unqualified(df)
+    final_data = process_final_data(clicks_output,conversion_output,qualified_output,unqualified_output)
+    return final_data
+
+# Lấy data từ bảng `company`
+def retrieve_company_data():
+    sql = """(SELECT id as job_id, company_id, group_id, campaign_id FROM job) test"""
+    company = spark.read.format('jdbc').options(url=MY_URL, driver=MY_DRIVER, dbtable=sql, user=MY_USER, password=MY_PASSWORD).load()
+    return company
+
+def import_to_mysql(output, db_table):
+    final_output = output.select('job_id','date','hour','publisher_id','company_id','campaign_id','group_id'\
+        ,'unqualified','qualified','conversions','clicks','bid_set','spend_hour', 'latest_update_time')
+    final_output = final_output\
+        .withColumnRenamed('date','dates')\
+        .withColumnRenamed('hour','hours')\
+        .withColumnRenamed('qualified','qualified_application')\
+        .withColumnRenamed('unqualified','disqualified_application')\
+        .withColumnRenamed('conversions','conversion')\
+        .withColumn('sources', lit('Cassandra'))
+    
+    # Import vào db
+    final_output.write.format('jdbc')\
+    .option('url', MY_URL)\
+    .option('driver', MY_DRIVER)\
+    .option('dbtable', db_table)\
+    .option('user', MY_USER)\
+    .option('password', MY_PASSWORD)\
+    .mode('append').save()
+    return print('Data imported successfully')
+
+# Main task
+def main_task():
+    print('The host is ' ,MY_HOST)
+    print('The port using is ',MY_PORT)
+    print('The db using is ',MY_DBNAME)
+    print('-----------------------------')
+    print('Retrieving and selecting data from Cassandra')
+    print('-----------------------------')
+    data = spark.read.format("org.apache.spark.sql.cassandra")\
+    .options(table=CAS_TABLE, keyspace=CAS_KEYSPACE)\
+    .load()\
+    .select('create_time', col('job_id').cast(IntegerType()).cast(StringType()), 'custom_track','bid','campaign_id'\
+            ,col('group_id').cast(IntegerType()).cast(StringType()), 'publisher_id', 'ts')
+    data.printSchema()
+    print('-----------------------------')
+    print('Processing data from Cassandra')
+    print('-----------------------------')
+    df = process_df(data)
+    print('-----------------------------')
+    print('Getting and check newest data')
+    print('-----------------------------')
+    df = df\
+        #.where(col('ts')>= mysql_time)
+    print('-----------------------------')
+    print('Processing Cassandra Output')
+    print('-----------------------------')
+    cassandra_output = process_cassandra_data(df)
+    print('-----------------------------')
+    print('Merge Company Data')
+    print('-----------------------------')
+    company = retrieve_company_data()
+    print('-----------------------------')
+    print('Finalizing Output')
+    print('-----------------------------')
+    final_output = cassandra_output\
+        .join(company,'job_id','full')\
+        .drop(company.group_id)\
+        .drop(company.campaign_id)\
+        .withColumn('latest_update_time', current_timestamp())
+    print('-----------------------------')
+    print('Import Output to MySQL')
+    print('-----------------------------')
+    import_to_mysql(final_output, db_table='events')
+    return print('Task Finished')
+
+if __name__ == "__main__":
+    spark = SparkSession.builder\
+    .appName('local')\
+    .config("spark.jars", "mysql-connector-java-8.0.30.jar")\
+    .config('spark.jars', 'spark-cassandra-connector-assembly_2.12-3.3.0.jar')\
+    .getOrCreate()
+
+    main()
